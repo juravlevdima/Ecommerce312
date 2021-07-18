edef80b707f79c18db0d0978361b9bb36b46ec3c
Dmitrii <69399561+juravlevdima@users.noreply.github.com>
2021-07-18 16:57:01 +0600

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import store from './redux/index.js'
+import App from './App.js'
+import './css/index.css' // tailwind compiled styles
+import './css/style.css' // custom styles
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
